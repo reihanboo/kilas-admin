2c3fba00994e78cac60aac0a7d7da68fd25287c3
Reihan Fahlevi <whenautumnend@gmail.com>
2026-05-01 14:27:51 +0700

    fix: added delete and options method to cors

--- backend/main.go
+++ backend/main.go
@@ -38,7 +38,7 @@ func main() {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, OPTIONS")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(204)
 			return
